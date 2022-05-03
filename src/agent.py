0980df4b6795084075c47d15d22582030deb09d1
Artur Khaialiev <a.khayaliev@innopolis.ru>
2022-05-03 21:06:15 +0200

    update agent

--- src/agent.py
+++ src/agent.py
@@ -1,35 +1,52 @@
-from forta_agent import Finding, FindingType, FindingSeverity, get_transaction_receipt, get_web3_provider, Web3
+from forta_agent import FindingSeverity, FindingType, Finding, get_transaction_receipt
+
+from .parser import update_pools_data as update_pools_data_function
+from .forecast import get_optimal_parameters_for_pool as get_optimal_parameters_for_pool_function
+from apscheduler.schedulers.background import BackgroundScheduler
 
 MEDIUM_GAS_THRESHOLD = 1000000
 HIGH_GAS_THRESHOLD = 3000000
 CRITICAL_GAS_THRESHOLD = 7000000
+findings_count = 0
 
-# MIN_BALANCE = int("500000000000000000")  # 0.5 eth
+
+def initialize():
+    update_pools_data_function()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_pools_data_function, 'interval', hours=24)
+    scheduler.add_job(get_optimal_parameters_for_pool_function, 'interval', days=28)
+    scheduler.start()
 
 
-def provide_handle_block(w3):
-    def handle_block(block_event):
+def provide_handle_transaction(get_transaction_receipt):
+    def handle_transaction(transaction_event):
+        # limiting this agent to emit only 5 findings so that the alert feed is not spammed
+        global findings_count
+        if findings_count >= 5:
+            return []
+
         findings = []
-        print(w3.eth.generate_gas_price())
-        # balance = int(w3.eth.get_balance(
-        #     Web3.toChecksumAddress(ACCOUNT), int(block_event.block_number)))
-        # if (balance >= MIN_BALANCE):
-        #     return findings
 
-        # findings.append(Finding({
-        #     'name': "Minimum Account Balance",
-        #     'description': f'Account balance ({balance}) below threshold ({MIN_BALANCE})',
-        #     'alert_id': "FORTA-6",
-        #     'severity': FindingSeverity.Info,
-        #     'type': FindingType.Suspicious,
-        #     'metadata': {
-        #         'account': ACCOUNT,
-        #         'balance': balance
-        #     }
-        # }))
+        receipt = get_transaction_receipt(transaction_event.hash)
+        gas_used = int(receipt.gas_used)
+        if gas_used < MEDIUM_GAS_THRESHOLD:
+            return findings
+
+        findings.append(Finding({
+            'name': 'High Gas Used',
+            'description': f'Gas Used: {gas_used}',
+            'alert_id': 'FORTA-1',
+            'type': FindingType.Suspicious,
+            'severity': get_severity(gas_used),
+            'metadata': {
+                'gas_used': gas_used
+            }
+        }))
+        findings_count += len(findings)
         return findings
 
-    return handle_block
+    return handle_transaction
+
 
 def get_severity(gas_used):
     if gas_used > CRITICAL_GAS_THRESHOLD:
@@ -40,8 +57,8 @@ def get_severity(gas_used):
         return FindingSeverity.Medium
 
 
-w3 = get_web3_provider()
-real_handle_block = provide_handle_block(w3)
+real_handle_transaction = provide_handle_transaction(get_transaction_receipt)
 
-def handle_block(block_event):
-    return real_handle_block(block_event)
+
+def handle_transaction(transaction_event):
+    return real_handle_transaction(transaction_event)
